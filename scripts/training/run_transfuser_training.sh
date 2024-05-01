59772d0ad6beb80acba9fb2068236d97697bdbe0
william <william@mychery.com>
2024-05-01 18:01:30 +0800

    for transfuser mini train

--- scripts/training/run_transfuser_training.sh
+++ scripts/training/run_transfuser_training.sh
@@ -1,5 +1,5 @@
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
 agent=transfuser_agent \
-experiment_name=training_transfuser_agent \
-scene_filter=navtrain \
-split=trainval \
+experiment_name=william_training_transfuser_agent \
+scene_filter=all_scenes \
+split=mini \
